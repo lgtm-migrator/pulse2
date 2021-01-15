8b8b911d5269f3a0b08b390e4e9709d17d31e456
William Vasselin <43633092+Hruund@users.noreply.github.com>
2021-01-15 17:48:57 +0100

    [jenkins] - test error git commit

--- tests/unittests/cache.py
+++ tests/unittests/cache.py
@@ -19,7 +19,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with MMC.  If not, see <http://www.gnu.org/licenses/>.
-#
+##
 """
 Test module for the Pulse 2 cache
 """
