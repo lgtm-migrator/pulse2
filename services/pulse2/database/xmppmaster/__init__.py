ece09177ac17f0f0024c4a1acde324dcceaf9134
botheis <noel.kneib@siveo.net>
2021-04-13 14:39:07 +0200

    [GLPI] Ignore empty or "" uuids for presence

--- services/pulse2/database/xmppmaster/__init__.py
+++ services/pulse2/database/xmppmaster/__init__.py
@@ -5668,7 +5668,7 @@ class XmppMasterDatabase(DatabaseHelper):
                     xmppmaster.machines
                  WHERE
                     enabled = '1' and
-                    agenttype = 'machine' and uuid_inventorymachine IS NOT NULL;"""
+                    agenttype = 'machine' and uuid_inventorymachine IS NOT NULL OR uuid_inventorymachine!='';"""
 
         presencelist = session.execute(sql)
         session.commit()
