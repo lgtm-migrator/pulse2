873c81f91a03dabec163de7b6a1b56f9476f6930
William Vasselin <william.vasselin@siveo.net>
2022-02-02 16:32:43 +0000

    Translation of variable name. Add traduction method for every array elements.

--- web/modules/urbackup/urbackup/index.php
+++ web/modules/urbackup/urbackup/index.php
@@ -3,26 +3,26 @@ require("graph/navbar.inc.php");
 require("localSidebar.php");
 require_once("modules/urbackup/includes/xmlrpc.php");
 
-$p = new PageGenerator(_T("Client machine list", 'pkgs'));
+$p = new PageGenerator(_T("Client machine list", 'urbackup'));
 $p->setSideMenu($sidemenu);
 $p->display();
 
-$tableau = xmlrpc_get_clients();
-$clients = $tableau["navitems"]["clients"];
+$client_array = xmlrpc_get_clients();
+$clients = $client_array["navitems"]["clients"];
 
 echo '<br>';
 echo '<br>';
 echo "<table style:'border: 1px solid #333;'>";
 echo '    <thead>';
 echo '        <tr>';
-echo '            <th colspan="3">Client list</th>';
+echo '            <th colspan="3">'._T("Clients", 'urbackup').'</th>';
 echo '        </tr>';
 echo '    </thead>';
 echo '    <tbody>';
 echo '        <tr>';
-echo "            <td style='padding:0px 500px 0px 0px;'>Client name</td>";
-echo '            <td>Group</td>';
-echo '            <td>Online</td>';
+echo "            <td style='padding:0px 500px 0px 0px;'>"._T("Clients name", 'urbackup')."</td>";
+echo '            <td>'._T("Group", 'urbackup').'</td>';
+echo '            <td>'._T("Online state", 'urbackup').'</td>';
 echo '        </tr>';
 
 foreach ($clients as $client) {
@@ -35,15 +35,4 @@ foreach ($clients as $client) {
 
 echo '    </tbody>';
 echo '</table>';
-echo '<br>';
-echo '<br>';
-
-echo '--------------------';
-echo '<br>';
-echo '<br>';
-
-echo 'Debug - CLIENTS Array';
-echo '<br>';
-
-print_r($clients);
 ?>
