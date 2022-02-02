6f64413bb0187947ef77d57b08842bf22dd733ed
William Vasselin <william.vasselin@siveo.net>
2022-02-02 17:02:40 +0000

    Syntax php and html correction on download client page

--- web/modules/urbackup/urbackup/downloads_client_urb.php
+++ web/modules/urbackup/urbackup/downloads_client_urb.php
@@ -7,11 +7,17 @@ $p = new PageGenerator(_T("Download page of Urbackup Client", 'urbackup'));
 $p->setSideMenu($sidemenu);
 $p->display();
 $windows_link = "https://hndl.urbackup.org/Client/2.4.11/UrBackup%20Client%202.4.11.exe";
-
-echo '<h2>'._T("Windows client", 'urbackup').'</h2>';
-echo "<button><a href='$windows_link' target='_blank'>Download</a></button>";
-echo '</br>';
-echo '<h2>'._T("Linux clients", 'urbackup').'</h2>';
-echo '<p>Run command :</p>';
-echo '<p>TF=$(mktemp) && wget "https://hndl.urbackup.org/Client/2.4.11/UrBackup%20Client%20Linux%202.4.11.sh" -O $TF && sudo sh $TF; rm -f $TF</p>';
 ?>
+
+<br>
+
+<h2> <?php echo _T("Windows client", 'urbackup'); ?> </h2>
+<?php echo "<button><a href='$windows_link' target='_blank'>Download</a></button>"; ?>
+
+<br>
+<br>
+
+<h2> <?php echo _T("Linux clients", 'urbackup'); ?> </h2>
+<p> <?php echo _T("Run command :", 'urbackup'); ?> </p>
+
+<p>TF=$(mktemp) && wget "https://hndl.urbackup.org/Client/2.4.11/UrBackup%20Client%20Linux%202.4.11.sh" -O $TF && sudo sh $TF; rm -f $TF</p>
