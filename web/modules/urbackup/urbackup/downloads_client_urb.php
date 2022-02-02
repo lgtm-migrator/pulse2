2ea66b52c6e38530b6a8251085a5c4d46f1dae93
William Vasselin <william.vasselin@siveo.net>
2022-02-02 16:37:07 +0000

    Translation of text and cleaning page and code

--- web/modules/urbackup/urbackup/downloads_client_urb.php
+++ web/modules/urbackup/urbackup/downloads_client_urb.php
@@ -3,17 +3,15 @@ require("graph/navbar.inc.php");
 require("localSidebar.php");
 require_once("modules/urbackup/includes/xmlrpc.php");
 
-$p = new PageGenerator(_T("Télécharger le client Windows", 'pkgs'));
+$p = new PageGenerator(_T("Download page of Urbackup Client", 'urbackup'));
 $p->setSideMenu($sidemenu);
 $p->display();
-$link_windows = "https://hndl.urbackup.org/Client/2.4.11/UrBackup%20Client%202.4.11.exe";
+$windows_link = "https://hndl.urbackup.org/Client/2.4.11/UrBackup%20Client%202.4.11.exe";
 
-echo '<h1>User session :</h1>';
-echo '<pre>';
-print_r(xmlrpc_get_session());
-echo '</pre>';
-echo "Windows client : <button><a href='$link_windows' target='_blank'>Download</a></button>";
+echo '<h2>'._T("Windows client", 'urbackup').'</h2>';
+echo "<button><a href='$windows_link' target='_blank'>Download</a></button>";
 echo '</br>';
-echo '<p>Linux Client : Run command :</p>';
+echo '<h2>'._T("Linux clients", 'urbackup').'</h2>';
+echo '<p>Run command :</p>';
 echo '<p>TF=$(mktemp) && wget "https://hndl.urbackup.org/Client/2.4.11/UrBackup%20Client%20Linux%202.4.11.sh" -O $TF && sudo sh $TF; rm -f $TF</p>';
 ?>
