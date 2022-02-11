afe4f0c45fa1acf50451c6b663bff50106f12e9f
William Vasselin <william.vasselin@siveo.net>
2022-02-11 15:14:49 +0000

    on index list client, we now use endpoint status from function get_status for get more information about client

--- web/modules/urbackup/urbackup/index.php
+++ web/modules/urbackup/urbackup/index.php
@@ -7,8 +7,8 @@ $p = new PageGenerator(_T("Clients machine list", 'urbackup'));
 $p->setSideMenu($sidemenu);
 $p->display();
 
-$client_array = xmlrpc_get_clients();
-$clients = $client_array["navitems"]["clients"];
+$client_array = xmlrpc_get_status();
+$clients = $client_array["status"];
 ?>
 <br>
 <br>
@@ -17,24 +17,32 @@ $clients = $client_array["navitems"]["clients"];
         <tr style='text-align: left; text-decoration: underline;'>
           <th> <?php echo _T("Name", 'urbackup'); ?> </th>
           <th> <?php echo _T("Group", 'urbackup'); ?> </th>
+          <th> <?php echo _T("OS", 'urbackup'); ?> </th>
+          <th> <?php echo _T("IP", 'urbackup'); ?> </th>
           <th> <?php echo _T("Online state", 'urbackup'); ?> </th>
         </tr>
     </thead>
     <tbody>
 <?php 
 foreach ($clients as $client) {
+    if ($client['groupname'] != "")
+    {
+        $group = $client['groupname'];
+    }
+    else
+    {
+        $group = _T("No group", 'urbackup');
+    }
 ?>
         <tr>
             <td style='padding:0px 500px 0px 0px;'> <?php echo $client['name']; ?></td>
-            <td> <?php echo $client['groupname']; ?></td>
-            <td>-</td>
+            <td> <?php echo $group; ?></td>
+            <td> <?php echo $client['os_simple']; ?></td>
+            <td> <?php echo $client['ip']; ?></td>
+            <td> <?php echo $client['online']; ?></td>
         </tr>
 <?php
 }
 ?>
     </tbody>
 </table>
-
-<?php
-print_r(xmlrpc_get_status());
-?>
