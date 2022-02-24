44c4b58fd4ba43b61e109e2030f65b45996f6d2e
William Vasselin <william.vasselin@siveo.net>
2022-02-24 19:19:37 +0000

    [urbackup] change title page

--- web/modules/urbackup/urbackup/start_backup.php
+++ web/modules/urbackup/urbackup/start_backup.php
@@ -3,12 +3,12 @@ require("graph/navbar.inc.php");
 require("localSidebar.php");
 require_once("modules/urbackup/includes/xmlrpc.php");
 
-$p = new PageGenerator(_T("List Backups by Client", 'urbackup'));
-$p->setSideMenu($sidemenu);
-$p->display();
-
 $type_backup = htmlspecialchars($_GET["backuptype"]);
 $client_id = htmlspecialchars($_GET["clientid"]);
+
+$p = new PageGenerator(_T("Start ".$type_backup." backup", 'urbackup'));
+$p->setSideMenu($sidemenu);
+$p->display();
 
 if ($type_backup == "incremental")
     $backup = xmlrpc_create_backup_incremental_file($client_id);
