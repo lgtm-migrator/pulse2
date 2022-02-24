b2d246425d9937abebcefa3ad4bc29c2d9ead890
William Vasselin <william.vasselin@siveo.net>
2022-02-24 16:56:57 +0000

    on save page, change table design and call different xmlrpc function

--- web/modules/urbackup/urbackup/saves.php
+++ web/modules/urbackup/urbackup/saves.php
@@ -7,8 +7,11 @@ $p = new PageGenerator(_T("Saves", 'urbackup'));
 $p->setSideMenu($sidemenu);
 $p->display();
 
-$backups_list_array = xmlrpc_get_backups();
+$backups_list_array = xmlrpc_get_backups_all_client();
 $clients_backup_list = $backups_list_array["clients"];
+
+$client_array = xmlrpc_get_status();
+$clients = $client_array["status"];
 
 //Formatage de date
 function secs2date($secs,$date)
@@ -27,18 +30,20 @@ function secs2date($secs,$date)
 
 <br>
 
-<table style:'border: 1px solid #333;'>
+
+<table class="listinfos" border="1px" cellspacing="0" cellpadding="5" >
     <thead>
-        <tr style='text-align: left; text-decoration: underline;'>
+        <tr style='text-align: left;'>
             <th> <?php echo _T("Clients name", 'urbackup'); ?> </th>
             <th> <?php echo _T("Last backup file", 'urbackup'); ?> </th>
+            <th> <?php echo _T("Action", 'urbackup'); ?> </th>
         </tr>
     </thead>
     <tbody>
 
 <?php
 foreach ($clients_backup_list as $client_backup) {
-
+    $id = $client_backup['id'];
     $date=new dateTime();
 
     $secs=$client_backup['lastbackup'];  //2033-12-06 08:53:20
@@ -46,9 +51,16 @@ foreach ($clients_backup_list as $client_backup) {
     $dt=$date->format('Y-m-d H:i:s');
 
 ?>
-        <tr>
-            <td style='padding:0px 500px 0px 0px;'> <?php echo $client_backup['name']; ?></td>
+        <tr class="alternate">
+            <td style='padding:0px 500px 0px 5px;'>
+                <a href="main.php?module=urbackup&amp;submod=urbackup&amp;action=list_backups&amp;clientid=<?php echo $id; ?>"><?php echo $client_backup['name']; ?></a>
+            </td>
             <td> <?php echo $dt; ?></td>
+            <td>
+            <li class="display">
+                <a title=<?php echo _T("Browse", 'urbackup'); ?> href="main.php?module=urbackup&amp;submod=urbackup&amp;action=list_backups&amp;clientid=<?php echo $id; ?>">&nbsp;</a>
+            </li>
+            </td>
         </tr>
 <?php
 }
