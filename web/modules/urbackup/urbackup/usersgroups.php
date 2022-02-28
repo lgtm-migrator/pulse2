f06f5bd0089588ca690c6469e2b2d75afaff2af7
William Vasselin <william.vasselin@siveo.net>
2022-02-28 21:56:23 +0000

    [urbackup] Remove user table on usersgroups page and add button to edit group Here now, we only have informations about groups, "actions" part on table group, we add Edit button to call edit_group_settings php page to save new settings

--- web/modules/urbackup/urbackup/usersgroups.php
+++ web/modules/urbackup/urbackup/usersgroups.php
@@ -3,7 +3,7 @@ require("graph/navbar.inc.php");
 require("localSidebar.php");
 require_once("modules/urbackup/includes/xmlrpc.php");
 
-$p = new PageGenerator(_T("Users and groups list", 'pkgs'));
+$p = new PageGenerator(_T("Groups", 'urbackup'));
 $p->setSideMenu($sidemenu);
 $p->display();
 
@@ -12,47 +12,13 @@ $users_group_array = xmlrpc_get_clients();
 
 <br>
 <br>
-<h1> <?php echo _T("Users list :", 'urbackup'); ?> </h1>
+<h1><?php echo _T("Create group :", "urbackup"); ?></h1>
 <br>
-<table class="listinfos" border="1px" cellspacing="0" cellpadding="5" >
-    <thead>
-        <tr>
-            <th style='text-align: left;'> <?php echo _T("User name", 'urbackup'); ?> </th>
-            <th style='text-align: left;'> <?php echo _T("Rights", 'urbackup'); ?> </th>
-            <th style='text-align: right;'> <?php echo _T("Actions", 'urbackup'); ?> </th>
-        </tr>
-    </thead>
-    <tbody>
+<form action="main.php?module=urbackup&amp;submod=urbackup&amp;action=create_group" method="POST">
+    <label>Group name :</label><input type="text" name="groupname" id="groupname"/>
+    <input type="submit" value="Create groupe">
+</form>
 
-<?php
-$user_array = $users_group_array['users'];
-
-foreach ($user_array as $user) {
-?>
-        <tr>
-            <td style='padding-left: 5px;'> <?php echo $user['name']; ?></td>
-            <td>
-            <?php
-            $user_rights = $user['rights'];
-
-            foreach ($user_rights as $rights) {
-                  echo $rights['domain'].': '.$rights['right'].'. ';
-            }
-            ?>
-            </td>
-            <td>
-            <ul class="action">
-                <li class="delete">
-                    <a title=<?php echo _T("Delete", 'urbackup'); ?> href="main.php?module=urbackup&amp;submod=urbackup&amp;action=delete" onclick="PopupWindow(event,'main.php?module=urbackup&amp;submod=urbackup&amp;action=delete', 300); return false;">&nbsp;</a>
-                </li>
-            </ul>
-            </td>
-        </tr>
-<?php
-}
-?>
-    </tbody>
-</table>
 <br>
 <br>
 
@@ -78,6 +44,9 @@ foreach ($group_array as $group) {
             <td style='padding-left: 5px;'> <?php echo $group['name']; ?></td>
             <td>
             <ul class="action">
+                <li class="edit">
+                    <a title=<?php echo _T("Edit", 'urbackup'); ?> href="main.php?module=urbackup&amp;submod=urbackup&amp;action=edit_group_settings&amp;groupid=<?php echo $group['id']; ?>&amp;groupname=<?php echo $group['name']; ?>">&nbsp;</a>
+                </li>
                 <li class="delete">
                     <a title=<?php echo _T("Delete", 'urbackup'); ?> href="main.php?module=urbackup&amp;submod=urbackup&amp;action=delete" onclick="PopupWindow(event,'main.php?module=urbackup&amp;submod=urbackup&amp;action=delete', 300); return false;">&nbsp;</a>
                 </li>
