f16df6a506fe646183c9eba72bcd1ee220d75194
William Vasselin <william.vasselin@siveo.net>
2022-02-28 14:50:07 +0000

    [urbackup] add informations for user on checkMachine page, remove debug print

--- web/modules/urbackup/urbackup/checkMachine.php
+++ web/modules/urbackup/urbackup/checkMachine.php
@@ -3,12 +3,12 @@ require("graph/navbar.inc.php");
 require("localSidebar.php");
 require_once("modules/urbackup/includes/xmlrpc.php");
 
-$p = new PageGenerator(_T("Check if machine exist", 'urbackup'));
-$p->setSideMenu($sidemenu);
-$p->display();
-
 $clientname = htmlspecialchars($_GET["cn"]);
 $jidMachine = htmlspecialchars($_GET["jid"]);
+
+$p = new PageGenerator(_T("Check if ".$clientname." exist", 'urbackup'));
+$p->setSideMenu($sidemenu);
+$p->display();
 
 $clients = xmlrpc_get_backups_all_client();
 $clients = $clients["clients"];
@@ -24,25 +24,28 @@ foreach ($clients as $client)
     else
         $exist = "false";
 }
-
-print_r($exist);
+?>
+<br>
+<?php
 
 if ($exist == "true")
 {
-    //$url = "main.php?module=urbackup&amp;submod=urbackup&amp;action=list_backups&amp;clientid=".$id;
-    //header("Location: $url");
-    echo "<a href='main.php?module=urbackup&amp;submod=urbackup&amp;action=list_backups&amp;clientid=".$id."'>OUAISS</a>";
+    header("Location: ".$_SERVER['REQUEST_URI']."main.php?module=urbackup&amp;submod=urbackup&amp;action=list_backups&amp;clientid=".$id);
+    exit();
+    echo "<a href='main.php?module=urbackup&amp;submod=urbackup&amp;action=list_backups&amp;clientid=".$id."'>Go to user backups</a>";
 }
 else
 {
-    $create_client = xmlrpc_add_client($clientname, $jidMachine);
-    print_r($create_client);
+    $create_client = xmlrpc_add_client($clientname);
+
+    if ($create_client["already_exists"] == "1")
+        print_r(_T("Utilisateur déjà existant, en cours de suppression.","urbackup"));
+    else
+    {
+        print_r(_T("Utilisateur créé.","urbackup"));
+        $check_client = xmlrpc_check_client($jidMachine, $create_client["new_clientid"], $create_client["new_authkey"]);
+    }
+        
+    
 }
-    
-    
-
 ?>
-
-<br>
-<br>
-<h1>CHECK MACHINE IF EXIST</h1>
