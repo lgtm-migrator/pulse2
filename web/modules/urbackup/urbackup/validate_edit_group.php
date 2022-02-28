56581a506d97b046a21f038a4112650510f1489e
William Vasselin <william.vasselin@siveo.net>
2022-02-28 21:54:35 +0000

    [urbackup] Add new page for send new settings for a group This new page gonna call API to send new settings for a group

---
+++ web/modules/urbackup/urbackup/validate_edit_group.php
@@ -0,0 +1,120 @@
+<?php
+require("graph/navbar.inc.php");
+require("localSidebar.php");
+require_once("modules/urbackup/includes/xmlrpc.php");
+
+$p = new PageGenerator(_T("Settings saved for ", 'urbackup'));
+$p->setSideMenu($sidemenu);
+$p->display();
+
+$ini_array = parse_ini_file("/etc/mmc/plugins/urbackup.ini");
+$username_urbackup = $ini_array['username'];
+$password_urbackup = $ini_array['password'];
+
+$group_id = htmlspecialchars($_GET["groupid"]);
+//$name_data = htmlspecialchars($_GET["namedata"]);
+//$value_data = htmlspecialchars($_GET["valuedata"]);
+$name_data = "update_freq_incr";
+$value_data = "18000";
+$group_id = "-".$group_id;
+
+//$users_group_array = xmlrpc_save_settings($group_id, $name_data, $value_data);
+//-----------------------------------START LOGIN FUNCTION
+$url = "https://wva.siveo.net/urbackup/x?a=login";
+
+$curlid = curl_init($url);
+
+curl_setopt($curlid, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curlid, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curlid, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curlid, CURLOPT_POST, true);
+curl_setopt($curlid, CURLOPT_RETURNTRANSFER, true);
+$datas = [
+'username'=>$username_urbackup,
+'password'=>$password_urbackup,
+'plainpw'=>1
+];
+
+$urlencoded = "";
+foreach($datas as $key=>$val){
+$urlencoded .= $key.'='.$val.'&';
+}
+rtrim($urlencoded, '&');
+
+curl_setopt($curlid, CURLOPT_POSTFIELDS, $urlencoded);
+$response = curl_exec($curlid);
+
+if (curl_errno($curlid)) 
+{
+    echo 'Requête échouée : '.curl_error($curlid).'<br>';
+    $result = [];
+}
+else
+{
+$result = (array)json_decode($response);
+}
+
+curl_close($curlid);
+
+if(isset($result['session'], $result['success']) && $result['success'] == 1){
+    $session = $result['session'];
+}
+//-----------------------------------END LOGIN
+
+//-----------------------------------START SAVE SETTINGS FUNCTION
+$url = "https://wva.siveo.net/urbackup/x?a=settings";
+$curlid = curl_init($url);
+
+curl_setopt($curlid, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curlid, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curlid, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curlid, CURLOPT_POST, true);
+curl_setopt($curlid, CURLOPT_RETURNTRANSFER, true);
+
+$datas = [
+    'sa'=>'clientsettings_save',
+    't_clientid'=>$group_id,
+    $name_data=>$value_data,
+    'overwrite'=>"true",
+    'ses'=>$session,
+];
+
+$urlencoded = "";
+foreach($datas as $key=>$val){
+$urlencoded .= $key.'='.$val.'&';
+}
+rtrim($urlencoded, '&');
+
+curl_setopt($curlid, CURLOPT_POSTFIELDS, $urlencoded);
+$response = curl_exec($curlid);
+
+if (curl_errno($curlid))
+{
+    echo 'Requête échouée : '.curl_error($curlid).'<br>';
+    $result = [];
+}
+else
+{
+    $result = (array)json_decode($response);
+}
+curl_close($curlid);
+
+$saving = $result;
+$array = json_decode(json_encode($saving), true);
+//-----------------------------------END SAVE SETTINGS
+?>
+<br>
+<label><?php echo _T("Interval for incremental file backup", "urbackup"); ?></label>
+<?php echo $array['update_freq_incr']; ?>
+<br>
+<label><?php echo _T("Interval for full file backups", "urbackup"); ?></label>
+<?php echo $array['update_freq_full']; ?>
+<br>
+<label><?php echo _T("Excluded files", "urbackup"); ?></label>
+<?php echo $array['exclude_files']; ?>
+<br>
+<label><?php echo _T("Included files", "urbackup"); ?></label>
+<?php echo $array['include_files']; ?>
+<br>
+<label><?php echo _T("Default directories to backup", "urbackup"); ?></label>
+<?php echo $array['default_dirs']; ?>
