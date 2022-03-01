74533f603b2995959d09c3d47c00101554e2c5b4
William Vasselin <william.vasselin@siveo.net>
2022-03-01 17:29:46 +0000

    [urbackup] Use array for validating settings save Here we create new array with different value saved for group settings. and for each value on array, we execute xmlrpc call to edit settings for group

--- web/modules/urbackup/urbackup/validate_edit_group.php
+++ web/modules/urbackup/urbackup/validate_edit_group.php
@@ -11,14 +11,24 @@ $ini_array = parse_ini_file("/etc/mmc/plugins/urbackup.ini");
 $username_urbackup = $ini_array['username'];
 $password_urbackup = $ini_array['password'];
 
+$interval_frequence_incremental_save = $_POST['update_freq_incr'];
+$interval_frequence_full_save = $_POST['update_freq_full'];
+$exclude_files = $_POST['exclude_files'];
+$include_files = $_POST['include_files'];
+$default_dirs = $_POST['default_dirs'];
+
 $group_id = htmlspecialchars($_GET["groupid"]);
-//$name_data = htmlspecialchars($_GET["namedata"]);
-//$value_data = htmlspecialchars($_GET["valuedata"]);
-$name_data = "update_freq_incr";
-$value_data = "18000";
+
+$settings_saver = array (
+    "update_freq_incr" => $interval_frequence_incremental_save,
+    "update_freq_full" => $interval_frequence_full_save,
+    "exclude_files" => $exclude_files,
+    "include_files" => $include_files,
+    "default_dirs" => $default_dirs,
+);
+
 $group_id = "-".$group_id;
 
-//$users_group_array = xmlrpc_save_settings($group_id, $name_data, $value_data);
 //-----------------------------------START LOGIN FUNCTION
 $url = "https://wva.siveo.net/urbackup/x?a=login";
 
@@ -62,47 +72,48 @@ if(isset($result['session'], $result['success']) && $result['success'] == 1){
 //-----------------------------------END LOGIN
 
 //-----------------------------------START SAVE SETTINGS FUNCTION
-$url = "https://wva.siveo.net/urbackup/x?a=settings";
-$curlid = curl_init($url);
 
-curl_setopt($curlid, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($curlid, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($curlid, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($curlid, CURLOPT_POST, true);
-curl_setopt($curlid, CURLOPT_RETURNTRANSFER, true);
 
-$datas = [
-    'sa'=>'clientsettings_save',
-    't_clientid'=>$group_id,
-    $name_data=>$value_data,
-    'overwrite'=>"true",
-    'ses'=>$session,
-];
+foreach ($settings_saver as $value => $item) {
+    $name_data = $value;
+    $value_data = $item;
 
-$urlencoded = "";
-foreach($datas as $key=>$val){
-$urlencoded .= $key.'='.$val.'&';
-}
-rtrim($urlencoded, '&');
+    $url = "https://wva.siveo.net/urbackup/x?a=settings";
+    $curlid = curl_init($url);
 
-curl_setopt($curlid, CURLOPT_POSTFIELDS, $urlencoded);
-$response = curl_exec($curlid);
+    curl_setopt($curlid, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($curlid, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curlid, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curlid, CURLOPT_POST, true);
+    curl_setopt($curlid, CURLOPT_RETURNTRANSFER, true);
 
-if (curl_errno($curlid))
-{
-    echo 'Requête échouée : '.curl_error($curlid).'<br>';
-    $result = [];
-}
-else
-{
+    $datas = [
+        'sa'=>'clientsettings_save',
+        't_clientid'=>$group_id,
+        $name_data=>$value_data,
+        'overwrite'=>"true",
+        'ses'=>$session,
+    ];
+
+    $urlencoded = "";
+    foreach($datas as $key=>$val){
+    $urlencoded .= $key.'='.$val.'&';
+    }
+    rtrim($urlencoded, '&');
+
+    curl_setopt($curlid, CURLOPT_POSTFIELDS, $urlencoded);
+    $response = curl_exec($curlid);
+
     $result = (array)json_decode($response);
-}
-curl_close($curlid);
 
-$saving = $result;
-$array = json_decode(json_encode($saving), true);
+    curl_close($curlid);
 
-$settings = $array['settings'];
+    $saving = $result;
+    $array = json_decode(json_encode($saving), true);
+
+    $settings = $array['settings'];
+}   
+
 //-----------------------------------END SAVE SETTINGS
 ?>
 <br>
