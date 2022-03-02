90ac13d3c296128725a2149ae2876a615cffcfc2
William Vasselin <william.vasselin@siveo.net>
2022-03-02 10:38:46 +0000

    [urbackup] Change some syntaxe and informations on editor for group settings

--- web/modules/urbackup/urbackup/edit_group_settings.php
+++ web/modules/urbackup/urbackup/edit_group_settings.php
@@ -12,11 +12,11 @@ $p->display();
 
 ?>
 <br>
-<form name="form" action="main.php?module=urbackup&amp;submod=urbackup&amp;action=validate_edit_group&amp;groupid=<?php echo $group_id; ?>" method="post">
-    <label><?php echo _T("Interval for incremental file backups", "urbackup"); ?></label><input type="text" name="update_freq_incr" id="update_freq_incr"/>
-    <label><?php echo _T("Interval for full file backups", "urbackup"); ?></label><input type="text" name="update_freq_full" id="update_freq_full"/>
-    <label><?php echo _T("Excluded files", "urbackup"); ?></label><input type="text" name="exclude_files" id="exclude_files"/>
-    <label><?php echo _T("Included files", "urbackup"); ?></label><input type="text" name="include_files" id="include_files"/>
-    <label><?php echo _T("Default directories to backup", "urbackup"); ?></label><input type="text" name="default_dirs" id="default_dirs"/>
+<form name="form" action="main.php?module=urbackup&amp;submod=urbackup&amp;action=validate_edit_group&amp;groupid=<?php echo $group_id; ?>&amp;groupname=<?php echo $group_name; ?>" method="post">
+    <label><?php echo _T("Interval for incremental file backups (hour)", "urbackup"); ?></label><input type="text" name="update_freq_incr" id="update_freq_incr"/><br>
+    <label><?php echo _T("Interval for full file backups (day)", "urbackup"); ?></label><input type="text" name="update_freq_full" id="update_freq_full"/><br>
+    <label><?php echo _T("Excluded files", "urbackup"); ?></label><input type="text" name="exclude_files" id="exclude_files"/><br>
+    <label><?php echo _T("Included files", "urbackup"); ?></label><input type="text" name="include_files" id="include_files"/><br>
+    <label><?php echo _T("Default directories to backup", "urbackup"); ?></label><input type="text" name="default_dirs" id="default_dirs"/><br>
     <input type="submit" value="Save">
 </form>
