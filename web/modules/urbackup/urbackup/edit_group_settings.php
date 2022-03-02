0caaadf18d94437117eb4b19053b06a5de30ba1c
William Vasselin <william.vasselin@siveo.net>
2022-03-02 15:23:13 +0000

    [urbackup] Add API Call to get informations about settings by group On placeholder of input type text, we add actual value for group select

--- web/modules/urbackup/urbackup/edit_group_settings.php
+++ web/modules/urbackup/urbackup/edit_group_settings.php
@@ -10,13 +10,101 @@ $p = new PageGenerator(_T("Settings for ".$group_name, 'urbackup'));
 $p->setSideMenu($sidemenu);
 $p->display();
 
+$group_id_new = "-".$group_id;
+
+$ini_array = parse_ini_file("/etc/mmc/plugins/urbackup.ini");
+$username_urbackup = $ini_array['username'];
+$password_urbackup = $ini_array['password'];
+
+//-----------------------------------START LOGIN FUNCTION
+$url = "https://wva.siveo.net/urbackup/x?a=login";
+
+$curlid = curl_init($url);
+
+curl_setopt($curlid, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curlid, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curlid, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curlid, CURLOPT_POST, true);
+curl_setopt($curlid, CURLOPT_RETURNTRANSFER, true);
+$datas = [
+    'username'=>$username_urbackup,
+    'password'=>$password_urbackup,
+    'plainpw'=>1
+];
+
+$urlencoded = "";
+foreach($datas as $key=>$val){
+$urlencoded .= $key.'='.$val.'&';
+}
+rtrim($urlencoded, '&');
+
+curl_setopt($curlid, CURLOPT_POSTFIELDS, $urlencoded);
+$response = curl_exec($curlid);
+
+if (curl_errno($curlid)) 
+{
+    echo 'Requête échouée : '.curl_error($curlid).'<br>';
+    $result = [];
+}
+else
+{
+$result = (array)json_decode($response);
+}
+
+curl_close($curlid);
+
+if(isset($result['session'], $result['success']) && $result['success'] == 1){
+    $session = $result['session'];
+}
+//-----------------------------------END LOGIN
+
+//-----------------------------------START SAVE SETTINGS FUNCTION
+
+$url = "https://wva.siveo.net/urbackup/x?a=settings";
+$curlid = curl_init($url);
+
+curl_setopt($curlid, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curlid, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curlid, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curlid, CURLOPT_POST, true);
+curl_setopt($curlid, CURLOPT_RETURNTRANSFER, true);
+
+$datas = [
+    'sa'=>'clientsettings_save',
+    't_clientid'=>$group_id_new,
+    'ses'=>$session,
+];
+
+$urlencoded = "";
+foreach($datas as $key=>$val){
+$urlencoded .= $key.'='.$val.'&';
+}
+rtrim($urlencoded, '&');
+
+curl_setopt($curlid, CURLOPT_POSTFIELDS, $urlencoded);
+$response = curl_exec($curlid);
+
+$result = (array)json_decode($response);
+
+curl_close($curlid);
+
+$res = $result;
+$array = json_decode(json_encode($res), true);
+
+$settings = $array['settings'];
+
+//-----------------------------------END SAVE SETTINGS
+
+$interval_incremental_backup = $settings['update_freq_incr']/3600;
+$interval_full_backup = $settings['update_freq_full']/86400;
+
 ?>
 <br>
 <form name="form" action="main.php?module=urbackup&amp;submod=urbackup&amp;action=validate_edit_group&amp;groupid=<?php echo $group_id; ?>&amp;groupname=<?php echo $group_name; ?>" method="post">
-    <label><?php echo _T("Interval for incremental file backups (hour)", "urbackup"); ?></label><input type="text" name="update_freq_incr" id="update_freq_incr"/><br>
-    <label><?php echo _T("Interval for full file backups (day)", "urbackup"); ?></label><input type="text" name="update_freq_full" id="update_freq_full"/><br>
-    <label><?php echo _T("Excluded files", "urbackup"); ?></label><input type="text" name="exclude_files" id="exclude_files"/><br>
-    <label><?php echo _T("Included files", "urbackup"); ?></label><input type="text" name="include_files" id="include_files"/><br>
-    <label><?php echo _T("Default directories to backup", "urbackup"); ?></label><input type="text" name="default_dirs" id="default_dirs"/><br>
+    <label><?php echo _T("Interval for incremental file backups (hour)", "urbackup"); ?></label><input placeholder="Actual value: <?php echo $interval_incremental_backup; ?>" type="text" name="update_freq_incr" id="update_freq_incr"/><br>
+    <label><?php echo _T("Interval for full file backups (day)", "urbackup"); ?></label><input placeholder="Actual value: <?php echo $interval_full_backup; ?>" type="text" name="update_freq_full" id="update_freq_full"/><br>
+    <label><?php echo _T("Excluded files", "urbackup"); ?></label><input placeholder="Actual value: <?php echo $settings['exclude_files']; ?>" type="text" name="exclude_files" id="exclude_files"/><br>
+    <label><?php echo _T("Included files", "urbackup"); ?></label><input placeholder="Actual value: <?php echo $settings['include_files']; ?>" type="text" name="include_files" id="include_files"/><br>
+    <label><?php echo _T("Default directories to backup", "urbackup"); ?></label><input placeholder="Actual value: <?php echo $settings['default_dirs']; ?>" type="text" name="default_dirs" id="default_dirs"/><br><br>
     <input type="submit" value="Save">
 </form>
