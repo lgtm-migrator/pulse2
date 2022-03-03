519cddf9232c4a0d27f87aeaae2cf785aa49d9d0
William Vasselin <william.vasselin@siveo.net>
2022-03-03 12:05:05 +0000

    [urbackup] Add new page on page list, add_member_togroup_aftercheck When we click on computer in computer xmpp view, we check if user exist or not, if not, add client and, we ask to user to add client on group.

--- web/modules/urbackup/infoPackage.inc.php
+++ web/modules/urbackup/infoPackage.inc.php
@@ -39,6 +39,10 @@ $page = new Page("add_member_togroup", _T('Assign member to group', 'urbackup'))
 $page->setFile("modules/urbackup/urbackup/add_member_togroup.php");
 $submod->addPage($page);
 
+$page = new Page("add_member_togroup_aftercheck", _T('Assign member to group after add client', 'urbackup'));
+$page->setFile("modules/urbackup/urbackup/add_member_togroup_aftercheck.php");
+$submod->addPage($page);
+
 $page = new Page("edit_group_settings", _T('Edit group', 'urbackup'));
 $page->setFile("modules/urbackup/urbackup/edit_group_settings.php");
 $submod->addPage($page);
