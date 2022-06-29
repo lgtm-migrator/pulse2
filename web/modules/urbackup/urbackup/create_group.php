3ce08a95818e7cb7b64edefbba8a943d4d351a9a
William Vasselin <william.vasselin@siveo.net>
2022-06-29 09:59:24 +0000

    Fix creating groupe same name and empty or "space" name Now, client can't create group if one group already exist with this name Client can't create group with empty name of name contain only "blank space" in it

--- web/modules/urbackup/urbackup/create_group.php
+++ web/modules/urbackup/urbackup/create_group.php
@@ -7,15 +7,38 @@ $p = new PageGenerator(_T("Group creation", 'urbackup'));
 $p->setSideMenu($sidemenu);
 $p->display();
 
+$users_group_array = xmlrpc_get_clients();
+
 $groupname = $_POST['groupname'];
 
-$create_group = xmlrpc_add_group($groupname);
+$group_array = $users_group_array['navitems']['groups'];
+
+$group_already_exist = "False";
+
+$need_name = "False";
+
+foreach ($group_array as $group) {
+    if ($group['name'] == $groupname) {
+        $group_already_exist = "True";
+    }
+}
+
+if ($group_already_exist == "False") 
+{
+    if(strlen(trim($_POST['groupname']))<=0){
+        $need_name = "True";
+    }
+    else
+    {
+        $create_group = xmlrpc_add_group($groupname);
+    }
+}
 
 ?>
 <br>
 <br>
 <?php
-$url = 'main.php?module=urbackup&submod=urbackup&action=usersgroups';
+$url = 'main.php?module=urbackup&submod=urbackup&action=usersgroups&groupalreadyexist='.$group_already_exist.'&groupname='.$groupname.'&needname='.$need_name.'';
 
 header("Location: ".$url);
 ?>
