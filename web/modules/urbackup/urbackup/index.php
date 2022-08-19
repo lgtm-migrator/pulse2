f7696de00a0e5f6625e6fc2e04a70da528d70f4f
William Vasselin <william.vasselin@siveo.net>
2022-08-19 14:27:52 +0000

    [Urbackup] Move logs on review page, just under reviews

--- web/modules/urbackup/urbackup/index.php
+++ web/modules/urbackup/urbackup/index.php
@@ -316,3 +316,68 @@ foreach ($array as $review) {
 ?>
     </tbody>
 </table>
+
+<br>
+<h2>Logs</h2>
+
+<?php
+$logs_global = xmlrpc_get_logs();
+$logs = $logs_global['logdata'];
+
+?>
+
+<table class="listinfos" border="1px" cellspacing="0" cellpadding="5" >
+    <thead>
+        <tr style='text-align: left;'>
+          <th> <?php echo _T("Id", 'urbackup'); ?> </th>
+          <th> <?php echo _T("Message", 'urbackup'); ?> </th>
+          <th> <?php echo _T("Time", 'urbackup'); ?> </th>
+        </tr>
+    </thead>
+    <tbody>
+<?php 
+foreach ($logs as $log)
+{
+    $date=new dateTime();
+
+    $secs=$log['time'];  //2033-12-06 08:53:20
+    secs2date($secs,$date);
+    $dt=$date->format('Y-m-d H:i:s');
+
+    $msg = "<td>".$log['msg']."</td>";
+
+    $need_show_msg = "True";
+
+    if (strpos($log['msg'], 'FATAL:') !== false) {
+        $msg = $log['msg'];
+        $msg = "<td class='log_error'>".$msg."</td>";
+    }
+
+    if (strpos($log['msg'], 'Backup failed') !== false) {
+        $msg = $log['msg'];
+        $msg = "<td class='log_error'>".$msg."</td>";
+    }
+
+    if (strpos($log['msg'], 'Backup failed because of disk problems') !== false) {
+        $msg = $log['msg'];
+        $msg = "<td class='log_error'>"._T("Backup failed because of disk problems, no space left on disk (see previous messages)")."</td>";
+    }
+
+    if (strpos($log['msg'], 'Loading files') !== false) {
+        $need_show_msg = "False";
+    }
+
+    if ($need_show_msg == "True")
+    {
+?>
+        <tr >
+            <td> <?php echo $log['id']; ?></td>
+            <?php echo $msg; ?>
+            <td> <?php echo $dt; ?></td>
+        </tr>
+<?php
+    }
+}
+?>
+    </tbody>
+</table>
